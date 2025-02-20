7450d8f37e7e6af8d3b7491756a1e045b48ee52b
Malcolm Okaya <71758217+mldokaya@users.noreply.github.com>
2025-02-20 18:51:22 -0500

    Remove unused function and minor formatting

--- src/main.c
+++ src/main.c
@@ -1,28 +1,24 @@
-/* USER CODE BEGIN Header */
 /**
-    ******************************************************************************
-    * @file           : main.c
-    * @brief          : Main program body
-    ******************************************************************************
-    * @attention
-    *
-    * Copyright (c) 2024 STMicroelectronics.
-    * All rights reserved.
-    *
-    * This software is licensed under terms that can be found in the LICENSE file
-    * in the root directory of this software component.
-    * If no LICENSE file comes with this software, it is provided AS-IS.
-    *
-    ******************************************************************************
-    */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
 #include "main.h"
 #include "gpio.h"
 #include "spi.h"
 
-/* Private function prototypes -----------------------------------------------*/
-void SystemClock_Config(void);
 void sysclk_ll_init(){
 
     LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
@@ -74,6 +70,7 @@ int main(void)
     gpio_init();
     spi_init();
     i2c_init();
+
     hpcd_USB_OTG_FS.Instance = USB_OTG_FS;
     hpcd_USB_OTG_FS.Init.dev_endpoints = 4;
     hpcd_USB_OTG_FS.Init.speed = PCD_SPEED_FULL;
@@ -84,6 +81,7 @@ int main(void)
     hpcd_USB_OTG_FS.Init.lpm_enable = DISABLE;
     hpcd_USB_OTG_FS.Init.vbus_sensing_enable = DISABLE;
     hpcd_USB_OTG_FS.Init.use_dedicated_ep1 = DISABLE;
+
     if (HAL_PCD_Init(&hpcd_USB_OTG_FS) != HAL_OK)
     {
         Error_Handler();
