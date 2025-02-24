ae69176bb6ca1eb32aa9fc889a2597f220986938
Malcolm Okaya <71758217+mldokaya@users.noreply.github.com>
2025-02-24 02:16:52 -0500

    Add comments and clean up sh1106 code

--- src/sh1106/sh1106.c
+++ src/sh1106/sh1106.c
@@ -1,33 +1,30 @@
 #include "sh1106.h"
-#include "FreeRTOS.h"
 
-void sh1106_init(struct sh1106_dev *ctx){
-    ctx->set(&ctx->rst, false);
-    ctx->delay(1);
-    ctx->set(&ctx->rst, true);
+void sh1106_init(struct sh1106_dev *sh1106){
+    sh1106->set(&sh1106->rst, false);
+    sh1106->delay(1);
+    sh1106->set(&sh1106->rst, true);
 }
 
-void sh1106_send_data(struct sh1106_dev *ctx, uint8_t *data, uint8_t n_bytes){
-    ctx->set(&ctx->a0, true);
-    ctx->write((void *)ctx, data, n_bytes);
+void sh1106_send_data(struct sh1106_dev *sh1106, uint8_t *data, uint8_t n_bytes){
+    sh1106->set(&sh1106->a0, true); // A0 = high for display data
+    sh1106->write((void *)sh1106, data, n_bytes);
 }
 
-void sh1106_send_cmd(struct sh1106_dev *ctx, uint8_t cmd){
-    ctx->set(&ctx->a0, false);
-    ctx->write(ctx, &cmd, 1);
+void sh1106_send_cmd(struct sh1106_dev *sh1106, uint8_t cmd){
+    sh1106->set(&sh1106->a0, false); // A0 = low for commands
+    sh1106->write(sh1106, &cmd, 1);
 }
 
-void sh1106_send_cmd_list(struct sh1106_dev *ctx, uint8_t *cmds, uint8_t n_cmds){
-    for(int i = 0; i < n_cmds; i++){
-        // printf("sending: %02X\n", cmds[i]);
-    }
-    ctx->write(ctx, cmds, n_cmds);
+void sh1106_send_cmd_list(struct sh1106_dev *sh1106, uint8_t *cmds, uint8_t n_cmds){
+    sh1106->set(&sh1106->a0, false); // A0 = low for commands
+    sh1106->write(sh1106, cmds, n_cmds);
 }
 
-void sh1106_set_col(struct sh1106_dev *ctx, uint8_t line){
-    line += 2;
-    sh1106_send_cmd(ctx, line & 0x0F);
-    sh1106_send_cmd(ctx, (line >> 4) | 0x10);
+void sh1106_set_col(struct sh1106_dev *sh1106, uint8_t line){
+    line += SH1106_COL_OFFSET; // Shifting to account for unused columns
+    sh1106_send_cmd(sh1106, line & 0x0F);
+    sh1106_send_cmd(sh1106, (line >> 4) | 0x10);
 }
 
 void sh1106_clear(uint8_t *buffer){
@@ -42,30 +39,30 @@ void sh1106_fill(uint8_t *buffer){
     }
 }
 
-void sh1106_update_region(struct sh1106_dev *ctx, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h){
+void sh1106_update_region(struct sh1106_dev *sh1106, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h){
     int start_page = y / 8;
     int end_page = (y + h - 1) / 8;
-    x += 2;
+    x += SH1106_COL_OFFSET; // Shifting to account for unused columns
     for(int i = start_page; i <= end_page; i++){
-        sh1106_send_cmd(ctx, 0xB0 + i);
-        sh1106_send_cmd(ctx, x & 0x0F);
-        sh1106_send_cmd(ctx, (x >> 4) | 0x10);
-        sh1106_send_data(ctx, &buffer[i * 128 + x - 2], w);
+        sh1106_send_cmd(sh1106, 0xB0 + i);
+        sh1106_send_cmd(sh1106, x & 0x0F);
+        sh1106_send_cmd(sh1106, (x >> 4) | 0x10);
+        sh1106_send_data(sh1106, &buffer[i * 128 + x - 2], w);
     }
 }
 
-void sh1106_clear_region(struct sh1106_dev *ctx, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h){
+void sh1106_clear_region(struct sh1106_dev *sh1106, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h){
     int start_page = y / 8;
     int end_page = (y + h) / 8;
-    x += 2;
+    x += SH1106_COL_OFFSET; // Shifting to account for unused columns
     for(int i = start_page; i < end_page; i++){
         for(int j = 0; j < w; j++){
             buffer[i * 128 + x + w] = 0x00;
         }
-        sh1106_send_cmd(ctx, 0xB0 + i);
-        sh1106_send_cmd(ctx, x & 0x0F);
-        sh1106_send_cmd(ctx, (x >> 4) | 0x10);
-        sh1106_send_data(ctx, &buffer[i * 128 + x - 2], w);
+        sh1106_send_cmd(sh1106, 0xB0 + i);
+        sh1106_send_cmd(sh1106, x & 0x0F);
+        sh1106_send_cmd(sh1106, (x >> 4) | 0x10);
+        sh1106_send_data(sh1106, &buffer[i * 128 + x - 2], w);
     }
 
 }
--- src/sh1106/sh1106.h
+++ src/sh1106/sh1106.h
@@ -1,17 +1,29 @@
 #ifndef SH1106_H
 #define SH1106_H
 
-#include <stdbool.h>
 #include "sh1106_defs.h"
 
-void sh1106_init(struct sh1106_dev *ctx);
-void sh1106_send_data(struct sh1106_dev *ctx, uint8_t *data, uint8_t n_bytes);
-void sh1106_send_cmd(struct sh1106_dev *ctx, uint8_t cmd);
-void sh1106_send_cmd_list(struct sh1106_dev *ctx, uint8_t *cmds, uint8_t n_cmds);
-void sh1106_set_col(struct sh1106_dev *ctx, uint8_t col);
+void sh1106_init(struct sh1106_dev *sh1106);
+
+// Send n bytes to display
+void sh1106_send_data(struct sh1106_dev *sh1106, uint8_t *data, uint8_t n_bytes);
+
+// Send a command to the display
+void sh1106_send_cmd(struct sh1106_dev *sh1106, uint8_t cmd);
+
+// Send multiple commands to the display
+void sh1106_send_cmd_list(struct sh1106_dev *sh1106, uint8_t *cmds, uint8_t n_cmds);
+
+// Set the column of the display
+void sh1106_set_col(struct sh1106_dev *sh1106, uint8_t col);
+
 void sh1106_clear(uint8_t *buffer);
 void sh1106_fill(uint8_t *buffer);
-void sh1106_update_region(struct sh1106_dev *ctx, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
-void sh1106_clear_region(struct sh1106_dev *ctx, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
+
+// Update a w x h region of the display starting at x,y
+void sh1106_update_region(struct sh1106_dev *sh1106, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
+
+// Clear a w x h region of the display starting at x,y
+void sh1106_clear_region(struct sh1106_dev *sh1106, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
 
 #endif
