63d3e37a55d3fd988046147796bfc27ec0b66b71
Malcolm Okaya <71758217+mldokaya@users.noreply.github.com>
2025-02-25 02:29:21 -0500

    Add comments to is31fl3731 code and rename issi_update_page to issi_switch_page for clarity

--- src/is31fl3731/is31fl3731.c
+++ src/is31fl3731/is31fl3731.c
@@ -1,50 +1,49 @@
 #include "is31fl3731.h"
 
-void issi_init_frame(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *data){
-    if(data == NULL){
+void issi_init_frame(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *state){
+    if(state == NULL){
         for(int i = 0; i < ISSI_BYTES_PER_FRAME; i++){
             issi->buffer[frame + i] = 0;
         }
     }
     else{
         for(int i = 0; i < ISSI_BYTES_PER_FRAME; i++){
-            issi->buffer[frame + i] = data[i];
+            issi->buffer[frame + i] = state[i];
         }
     }
 }
 
-void issi_update_page(struct is31fl3731_dev *issi, enum issi_page page){
+void issi_switch_page(struct is31fl3731_dev *issi, enum issi_page page){
     if(issi->current_page != page){
         issi->current_page = page;
         issi->write_single(issi->i2c, ISSI_CMD, page);
     }
 }
 
+void issi_update_frame(struct is31fl3731_dev *issi, uint8_t frame){
+    issi->write_buf(issi->i2c, ISSI_CA1, &issi->buffer[frame * ISSI_BYTES_PER_FRAME], ISSI_BYTES_PER_FRAME);
+    issi_switch_page(issi, frame);
+}
+
+void issi_update_leds(struct is31fl3731_dev *issi, uint8_t frame, uint8_t reg, uint8_t n_regs){
+    issi->write_buf(issi->i2c, reg, &issi->buffer[frame * ISSI_BYTES_PER_FRAME + reg], n_regs);
+}
+
+void issi_update_functions(struct is31fl3731_dev *issi, enum issi_func func, uint8_t n_funcs){
+    issi_switch_page(issi, ISSI_FUNCTION);
+    issi->write_buf(issi->i2c, func, &issi->buffer[ISSI_FUNC_OFFSET + func], n_funcs);
+}
+
 void issi_set_led(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, bool state, bool update){
-    if(state){
-        issi->buffer[frame * ISSI_BYTES_PER_FRAME + 2 * y + x / 8] |= 1 << (x % 8);
-    }
-    else{
-        issi->buffer[frame * ISSI_BYTES_PER_FRAME + 2 * y + x / 8] &= ~(1 << (x % 8));
-    }
+    // Get the buffer data for this led and update the relevant bit with its new state
+    uint8_t *reg = &issi->buffer[ISSI_LED_INDEX(frame, x, y)];
+    *reg ^= (-state ^ *reg) & (1 << (x % 8));
     if(update){
         issi_update_leds(issi, frame, 2 * y + x / 8, 1);
     }
 }
 
-void issi_set_blink(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, bool state, bool update){
-    if(state){
-        issi->buffer[frame * ISSI_BYTES_PER_FRAME + 2 * y + x / 8 + 0x12] |= 1 << (x % 8);
-    }
-    else{
-        issi->buffer[frame * ISSI_BYTES_PER_FRAME + 2 * y + x / 8 + 0x12] &= ~(1 << (x % 8));
-    }
-    if(update){
-        issi_update_leds(issi, frame, 2 * y + x / 8 + 0x12, 1);
-    }
-}
-
-void issi_set_led_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update){
+void issi_set_led_group(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update){
     if(state != NULL){
         for(int i = 0; i < ISSI_LED_COUNT; i++){
             uint8_t *reg = &issi->buffer[ISSI_LED_INDEX(frame, x[i], y[i])];
@@ -66,7 +65,15 @@ void issi_set_led_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, ui
     }
 }
 
-void issi_set_blink_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update){
+void issi_set_blink(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, bool state, bool update){
+    uint8_t *reg = &issi->buffer[ISSI_BLINK_INDEX(frame, x, y)];
+    *reg ^= (-state ^ *reg) & (1 << (x % 8));
+    if(update){
+        issi_update_leds(issi, frame, 2 * y + x / 8 + 0x12, 1);
+    }
+}
+
+void issi_set_blink_group(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update){
     if(state != NULL){
         for(int i = 0; i < ISSI_LED_COUNT; i++){
             uint8_t *reg = &issi->buffer[ISSI_BLINK_INDEX(frame, x[i], y[i])];
@@ -88,10 +95,14 @@ void issi_set_blink_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, 
     }
 }
 
-void issi_set_pwm_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t *state, bool update){
-    if(state != NULL){
+void issi_set_pwm(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, uint8_t val, bool update){
+    issi->buffer[ISSI_PWM_INDEX(frame, x, y)] = val;
+}
+
+void issi_set_pwm_group(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t *val, bool update){
+    if(val != NULL){
         for(int i = 0; i < ISSI_LED_COUNT; i++){
-            issi->buffer[ISSI_PWM_INDEX(frame, x[i], y[i])] = state[i];
+            issi->buffer[ISSI_PWM_INDEX(frame, x[i], y[i])] = val[i];
         }
         if(update){
             issi_update_frame(issi, frame);
@@ -99,33 +110,19 @@ void issi_set_pwm_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x,
     }
 }
 
-void issi_set_pwm_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t state, bool update){
+void issi_set_pwm_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t val, bool update){
     for(int i = 0; i < ISSI_LED_COUNT; i++){
-        issi->buffer[ISSI_PWM_INDEX(frame, x[i], y[i])] = state;
+        issi->buffer[ISSI_PWM_INDEX(frame, x[i], y[i])] = val;
     }
     if(update){
         issi_update_frame(issi, frame);
     }
 }
 
-void issi_update_frame(struct is31fl3731_dev *issi, uint8_t frame){
-    issi_update_page(issi, frame);
-    issi->write_buf(issi->i2c, ISSI_CA1, &issi->buffer[frame * ISSI_BYTES_PER_FRAME], ISSI_BYTES_PER_FRAME);
-}
-
-void issi_update_leds(struct is31fl3731_dev *issi, uint8_t frame, uint8_t reg, uint8_t len){
-    issi->write_buf(issi->i2c, reg, &issi->buffer[frame * ISSI_BYTES_PER_FRAME + reg], len);
-}
-
-void issi_update_function(struct is31fl3731_dev *issi, enum issi_func func, uint8_t len){
-    issi_update_page(issi, ISSI_FUNCTION);
-    issi->write_buf(issi->i2c, func, &issi->buffer[ISSI_FUNC_OFFSET + func], len);
-}
-
 void issi_set_function(struct is31fl3731_dev *issi, enum issi_func func, uint8_t val, bool update){
     issi->buffer[ISSI_FUNC_OFFSET + func] = val;
     if(update){
-        issi_update_function(issi, func, 1);
+        issi_update_functions(issi, func, 1);
     }
 }
 
@@ -134,6 +131,6 @@ void issi_set_functions(struct is31fl3731_dev *issi, enum issi_func *funcs, uint
         issi->buffer[ISSI_FUNC_OFFSET + funcs[i]] = vals[i];
     }
     if(update){
-        issi_update_function(issi, ISSI_FUNC_CONFIG, ISSI_FUNCTIONS);
+        issi_update_functions(issi, ISSI_FUNC_CONFIG, ISSI_FUNCTIONS); // There aren't many functions, so might as well just update them all
     }
 }
--- src/is31fl3731/is31fl3731.h
+++ src/is31fl3731/is31fl3731.h
@@ -3,32 +3,162 @@
 
 #include "is31fl3731_defs.h"
 
-void issi_init_frame(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *data);
+/**
+ * @brief Sets the initial state of a frame in the buffer
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame to be set
+ * @param state State the frame should be set to, or NULL for all 0s 
+ */
+void issi_init_frame(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *state);
 
-void issi_update_page(struct is31fl3731_dev *issi, enum issi_page page);
+/**
+ * @brief Switches to the given page if it's not already on it
+ * @param issi Struct for storing buffer and IO functions
+ * @param page Page to switch to
+ */
+void issi_switch_page(struct is31fl3731_dev *issi, enum issi_page page);
 
-void issi_update_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t n_leds);
-
+/**
+ * @brief Updates the data for the given frame with the buffer data
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame to be updated
+ * @note This might change later, but for now the frame being updated is always the frame being displayed
+ */
 void issi_update_frame(struct is31fl3731_dev *issi, uint8_t frame);
 
-void issi_update_leds(struct is31fl3731_dev *issi, uint8_t frame, uint8_t reg, uint8_t len);
+/**
+ * @brief Updates the states of n consecutive led registers
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame to be displayed
+ * @param reg First register to update
+ * @param n_regs Number of consecutive registers to update
+ */
+void issi_update_leds(struct is31fl3731_dev *issi, uint8_t frame, uint8_t reg, uint8_t n_regs);
 
-void issi_update_function(struct is31fl3731_dev *issi, enum issi_func func, uint8_t len);
+/**
+ * @brief Updates n consecutive function registers
+ * @param issi Struct for storing buffer and IO functions
+ * @param func First function to update
+ * @param n_funcs Number of consecutive functions to update
+ */
+void issi_update_functions(struct is31fl3731_dev *issi, enum issi_func func, uint8_t n_funcs);
 
+/**
+ * @brief Sets the state for an individual LED
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the LED state should be set for
+ * @param x X-coordinate of the LED
+ * @param y Y-coordinate of the LED
+ * @param state State for the LED
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_led(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, bool state, bool update);
-void issi_set_led_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update);
+
+/**
+ * @brief Sets the states for a group of LEDs
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the LED state should be set for
+ * @param x X-coordinates for the group of LEDs
+ * @param y Y-coordinates for the group of LEDs
+ * @param state State for each LED in the group
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
+void issi_set_led_group(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update);
+
+/**
+ * @brief Sets a group of LEDs to the same state
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the LED state should be set for
+ * @param x X-coordinates for the group of LEDs
+ * @param y Y-coordinates for the group of LEDs
+ * @param state State for all LEDs in the group
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_led_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool state, bool update);
 
+/**
+ * @brief Sets the blink state for an individual LED
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the blink state should be set for
+ * @param x X-coordinate of the LED
+ * @param y Y-coordinate of the LED
+ * @param state State for the LED
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_blink(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, bool state, bool update);
-void issi_set_blink_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update);
+
+/**
+ * @brief Sets the blink states for a group of LEDs
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the blink state should be set for
+ * @param x X-coordinates for the group of LEDs
+ * @param y Y-coordinates for the group of LEDs
+ * @param state State for each LED in the group
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
+void issi_set_blink_group(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool *state, bool update);
+
+/**
+ * @brief Sets a group of LEDs to the same blink state
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the blink state should be set for
+ * @param x X-coordinates for the group of LEDs
+ * @param y Y-coordinates for the group of LEDs
+ * @param state State for all LEDs in the group
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_blink_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, bool state, bool update);
 
+/**
+ * @brief Sets the PWM value for an individual LED
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the pwm value should be set for
+ * @param x X-coordinate of the LED
+ * @param y Y-coordinate of the LED
+ * @param val PWM value for the LED
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_pwm(struct is31fl3731_dev *issi, uint8_t frame, uint8_t x, uint8_t y, uint8_t val, bool update);
-void issi_set_pwm_region(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t *state, bool update);
-void issi_set_pwm_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t state, bool update);
 
+/**
+ * @brief Sets the PWM values for a group of LEDs
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the pwm value should be set for
+ * @param x X-coordinates for the group of LEDs
+ * @param y Y-coordinates for the group of LEDs
+ * @param state PWM value for each LED in the group
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
+void issi_set_pwm_group(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t *val, bool update);
+
+/**
+ * @brief Sets a group of LEDs to the same PWM value
+ * @param issi Struct for storing buffer and IO functions
+ * @param frame Frame the pwm value should be set for
+ * @param x X-coordinates for the group of LEDs
+ * @param y Y-coordinates for the group of LEDs
+ * @param state PWM value for all LEDs in the group
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
+void issi_set_pwm_all(struct is31fl3731_dev *issi, uint8_t frame, uint8_t *x, uint8_t *y, uint8_t val, bool update);
+
+/**
+ * @brief Sets the value for a function
+ * @param issi Struct for storing buffer and IO functions
+ * @param func Function to set
+ * @param val Value to set for the function
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_function(struct is31fl3731_dev *issi, enum issi_func func, uint8_t val, bool update);
 
+/**
+ * @brief Sets the values for multiple functions
+ * @param issi Struct for storing buffer and IO functions
+ * @param funcs Functions to set
+ * @param vals Values to set for each function
+ * @param n_funcs Number of functions to set
+ * @param update Send new data to driver now (true) or only change the buffer (false)
+ */
 void issi_set_functions(struct is31fl3731_dev *issi, enum issi_func *funcs, uint8_t *vals, uint8_t n_funcs, bool update);
 
 #endif
