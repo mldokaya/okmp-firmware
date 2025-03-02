e41fe2e506c88ce895ad13addaa70c68a45b3469
Malcolm Okaya <71758217+mldokaya@users.noreply.github.com>
2025-03-02 12:30:56 -0500

    Add comments for sh1106 functions

--- src/sh1106/sh1106.h
+++ src/sh1106/sh1106.h
@@ -3,27 +3,74 @@
 
 #include "sh1106_defs.h"
 
+/**
+ * @brief Sets the display to its initial state
+ * @param sh1106 Struct for storing buffer and IO functions
+ */
 void sh1106_init(struct sh1106_dev *sh1106);
 
-// Send n bytes to display
+/**
+ * @brief Sends data to the display
+ * @param sh1106 Struct for storing buffer and IO functions
+ * @param data Pointer to data
+ * @param n_bytes Number of data bytes to transmit
+ */
 void sh1106_send_data(struct sh1106_dev *sh1106, uint8_t *data, uint8_t n_bytes);
 
-// Send a command to the display
+/**
+ * @brief Sends a command to the display
+ * @param sh1106 Struct for storing buffer and IO functions
+ * @param cmd Command to be sent
+ */
 void sh1106_send_cmd(struct sh1106_dev *sh1106, uint8_t cmd);
 
-// Send multiple commands to the display
+/**
+ * @brief Sends multiple commands to the display
+ * @param sh1106 Struct for storing buffer and IO functions
+ * @param cmds Array of commands to send
+ * @param n_cmds Number of commands being sent
+ */
 void sh1106_send_cmd_list(struct sh1106_dev *sh1106, uint8_t *cmds, uint8_t n_cmds);
 
-// Set the column of the display
+/**
+ * @brief Sets the column display data will be written to
+ * @param sh1106 Struct for storing buffer and IO functions
+ * @param col Column to set
+ */
 void sh1106_set_col(struct sh1106_dev *sh1106, uint8_t col);
 
+/**
+ * @brief Clears a buffer (does not actually update the display driver though)
+ * @param buffer Buffer to be cleared
+ */
 void sh1106_clear(uint8_t *buffer);
+
+/**
+ * @brief Fills a buffer (does not actually update the display driver though)
+ * @param buffer Buffer to be filled
+ */
 void sh1106_fill(uint8_t *buffer);
 
-// Update a w x h region of the display starting at x,y
+/**
+ * @brief Updates a rectangular w x h region of the display
+ * @param sh1106 Struct for storing buffer and IO functions
+ * @param buffer Buffer to use for updating
+ * @param x X-coordinate for the upper left corner of the region
+ * @param y X-coordinate for the upper left corner of the region
+ * @param w Width of the region
+ * @param h Height of the region
+ */
 void sh1106_update_region(struct sh1106_dev *sh1106, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
 
-// Clear a w x h region of the display starting at x,y
+/**
+ * @brief Clears a rectangular w x h region of the display
+ * @param sh1106 Struct for storing buffer and IO functions
+ * @param buffer Buffer to keep updated
+ * @param x X-coordinate for the upper left corner of the region
+ * @param y X-coordinate for the upper left corner of the region
+ * @param w Width of the region
+ * @param h Height of the region
+ */
 void sh1106_clear_region(struct sh1106_dev *sh1106, uint8_t *buffer, uint8_t x, uint8_t y, uint8_t w, uint8_t h);
 
 #endif
