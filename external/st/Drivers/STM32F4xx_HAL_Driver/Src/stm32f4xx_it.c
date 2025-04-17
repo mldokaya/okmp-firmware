5ae165080f901774bfe6fac332e2908dbcc823c9
Malcolm Okaya <71758217+mldokaya@users.noreply.github.com>
2025-04-17 19:59:57 -0400

    Add missing header

--- external/st/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_it.c
+++ external/st/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_it.c
@@ -21,6 +21,7 @@
 #include "main.h"
 #include "stm32f4xx_it.h"
 #include "tusb.h"
+#include "printf.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 /* USER CODE END Includes */
