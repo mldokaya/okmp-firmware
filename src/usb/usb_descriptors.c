c6632fa0ac8022c91d8b01c9483620a4f9e5a743
Malcolm Okaya <71758217+mldokaya@users.noreply.github.com>
2025-04-17 20:36:38 -0400

    Update USB report descriptor to add support for media controls

--- src/usb/usb_descriptors.c
+++ src/usb/usb_descriptors.c
@@ -83,38 +83,47 @@ uint8_t const * tud_descriptor_device_cb(void)
 // };
 
 uint8_t const desc_hid_report[] = {
-  0x05, 0x01,                     // Usage Page (Generic Desktop),
-  0x09, 0x06,                     // Usage (Keyboard),
+  0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+  0x09, 0x06,        // Usage (Keyboard)
+  0xA1, 0x01,        // Collection (Application)
+  0x75, 0x01,        //   Report Size (1)
+  0x95, 0x08,        //   Report Count (8)
+  0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+  0x19, 0xE0,        //   Usage Minimum (0xE0)
+  0x29, 0xE7,        //   Usage Maximum (0xE7)
+  0x15, 0x00,        //   Logical Minimum (0)
+  0x25, 0x01,        //   Logical Maximum (1)
+  0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  0x95, 0x05,        //   Report Count (5)
+  0x75, 0x01,        //   Report Size (1)
+  0x05, 0x08,        //   Usage Page (LEDs)
+  0x19, 0x01,        //   Usage Minimum (Num Lock)
+  0x29, 0x05,        //   Usage Maximum (Kana)
+  0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  0x95, 0x01,        //   Report Count (1)
+  0x75, 0x03,        //   Report Size (3)
+  0x91, 0x03,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+  0x95, 0x78,        //   Report Count (120)
+  0x75, 0x01,        //   Report Size (1)
+  0x15, 0x00,        //   Logical Minimum (0)
+  0x25, 0x01,        //   Logical Maximum (1)
+  0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+  0x19, 0x00,        //   Usage Minimum (0x00)
+  0x29, 0x65,        //   Usage Maximum (0x65)
+  0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+  0xC0,              // End Collection
+  0x05, 0x0C,                     // Usage Page (Consumer),
+  0x09, 0x01,                     // Usage (Consumer Control),
   0xA1, 0x01,                     // Collection (Application),
-  // bitmap of modifiers
-  0x75, 0x01,                     //   Report Size (1),
-  0x95, 0x08,                     //   Report Count (8),
-  0x05, 0x07,                     //   Usage Page (Key Codes),
-  0x19, 0xE0,                     //   Usage Minimum (224),
-  0x29, 0xE7,                     //   Usage Maximum (231),
+  0x85, 0x02,                     //   Report ID (2) - Consumer Control
   0x15, 0x00,                     //   Logical Minimum (0),
   0x25, 0x01,                     //   Logical Maximum (1),
-  0x81, 0x02,                     //   Input (Data, Variable, Absolute), ;Modifier byte
-  // LED output report
-  0x95, 0x05,                     //   Report Count (5),
   0x75, 0x01,                     //   Report Size (1),
-  0x05, 0x08,                     //   Usage Page (LEDs),
-  0x19, 0x01,                     //   Usage Minimum (1),
-  0x29, 0x05,                     //   Usage Maximum (5),
-  0x91, 0x02,                     //   Output (Data, Variable, Absolute),
-  0x95, 0x01,                     //   Report Count (1),
-  0x75, 0x03,                     //   Report Size (3),
-  0x91, 0x03,                     //   Output (Constant),
-  // bitmap of keys
-  0x95, 120,	//   Report Count (),
-  0x75, 0x01,                     //   Report Size (1),
-  0x15, 0x00,                     //   Logical Minimum (0),
-  0x25, 0x01,                     //   Logical Maximum(1),
-  0x05, 0x07,                     //   Usage Page (Key Codes),
-  0x19, 0x00,                     //   Usage Minimum (0),
-  0x29, 0x65,	//   Usage Maximum (),
+  0x95, 0x02,                     //   Report Count (16), (Adjust count as needed)
+  0x0A, 0xE9, 0x00,               //   Usage (Volume Up)
+  0x0A, 0xEA, 0x00,               //   Usage (Volume Down)
   0x81, 0x02,                     //   Input (Data, Variable, Absolute),
-  0xc0      
+  0xC0     
 };
 
 // Invoked when received GET HID REPORT DESCRIPTOR
